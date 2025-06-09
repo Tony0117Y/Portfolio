fd6c65cbf4708ba96ca7728403572407c72a5342
Tony <ty030117@gmail.com>
2025-06-09 17:05:48 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://Tony0117Y.github.io/Portfolio",
+  site: "https://www.tyang.dev",
   base: "/",
 });
